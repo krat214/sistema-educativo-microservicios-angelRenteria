d2cc150136deca93704c5d81b55626128506f54c
Angel Renteria <arenteria@recpeck.co>
2025-04-19 17:36:03 -0500

    up

--- matriculas-servicio/src/test/java/com/angelRenteria/matriculas_servicio/MatriculasServicioApplicationTests.java
+++ matriculas-servicio/src/test/java/com/angelRenteria/matriculas_servicio/MatriculasServicioApplicationTests.java
@@ -4,9 +4,11 @@ import com.angelRenteria.matriculas_servicio.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(TestConfig.class)
+@ActiveProfiles("test")
 class MatriculasServicioApplicationTests {
 
 	@Test
